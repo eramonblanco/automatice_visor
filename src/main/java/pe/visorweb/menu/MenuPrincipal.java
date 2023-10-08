7c7910bb433e66f6812c9ace9b40f8ed1efd7100
eramonblanco <eramonb@gmail.com>
2023-10-07 23:53:09 -0500

    defecto

--- src/main/java/pe/visorweb/menu/MenuPrincipal.java
+++ src/main/java/pe/visorweb/menu/MenuPrincipal.java
@@ -12,7 +12,7 @@ public class MenuPrincipal {
 	@FindBy(xpath = "//div[@class=\"menu\"]/div[1]")
 	private WebElement menuHamburguesa;
 	
-	@FindBy(xpath = "//span[contains(text(), \"Mod. de Almacen\")]")
+	@FindBy(xpath = "//span[contains(text(), \"Mod. de Almac&eacute;n\")]")
 	private WebElement menuModuloAlmacen;
 	
 	@FindBy(linkText = "Mnt. de Categoría")
